9b5ff23601f8e6fc8a0cabd42b56a534a1677ea2
Nicolas Widart <n.widart@gmail.com>
2016-07-29 15:06:19 +0200

    Using better form name structure

--- Resources/views/admin/types/translatable/textarea.blade.php
+++ Resources/views/admin/types/translatable/textarea.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group {{ $errors->has("attributes.{$attribute->key}.$locale") ? 'has-error' : '' }}">
-    {!! Form::label("attributes[][$attribute->key][$locale]", $attribute->name) !!}
-    {!! Form::textarea("attributes[][$attribute->key][$locale]", old($attribute->key), ['class' => 'form-control']) !!}
+    {!! Form::label("attributes[$attribute->key][$locale]", $attribute->name) !!}
+    {!! Form::textarea("attributes[$attribute->key][$locale]", old($attribute->key), ['class' => 'form-control']) !!}
     {!! $errors->first("attributes.{$attribute->key}.$locale", '<span class="help-block">:message</span>') !!}
 </div>
